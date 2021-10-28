4790736b2f54a90b579f096f4b22d9e3819fac0a
melisagurdal <80177011+melisagurdal@users.noreply.github.com>
2021-10-28 21:33:01 +0300

    app logo added and some fixes

--- App.js
+++ App.js
@@ -1,113 +1,130 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Modal } from 'react-native';
 import axios from 'axios';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 const API = "https://rickandmortyapi.com/api/"
+
+
+
 class App extends Component {
 
+    constructor(props) {
+        super(props);
 
-	constructor(props) {
-		super(props);
+        this.state = {
+            characters: [],
+            currentPage: 1,
+            totalPages: 0
+        }
+    }
 
-		this.state = {
-			characters: [],
-			currentPage: 1,
-			totalPages: 0
-		}
-	}
+    getCharacters(page = 1) {
+        axios.get(`${API}character/?page=${page}`)
+            .then((response) => {
+                let newData = this.state.characters;
+                let data = response.data && response.data.results || [];
 
-	getCharacters(page = 1) {
-		axios.get(`${API}character/?page=${page}`)
-			.then((response) => {
-				let newData = this.state.characters;
-				let data = response.data && response.data.results || [];
+                data.map((data) => {
+                    newData.push(data);
+                })
 
-				data.map((data) => {
-					newData.push(data);
-				})
+                this.setState({
+                    characters: newData,
+                    totalPages: response.data && response.data.info.pages || 0,
+                    currentPage: page
+                })
+            })
+            .catch((error) => {
+                console.log("error", error)
+            })
+    }
 
-				this.setState({
-					characters: newData,
-					totalPages: response.data && response.data.info.pages || 0,
-					currentPage: page
-				})
-			})
-			.catch((error) => {
-				console.log("error", error)
-			})
-	}
+    componentDidMount() {
+        this.getCharacters()
+    }
 
-	componentDidMount() {
-		this.getCharacters()
-	}
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.header}>Characters</Text>
+                <FlatList
+                    columnWrapperStyle={{ justifyContent: 'space-between' }}
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.header}>Characters</Text>
-				<FlatList
-				 columnWrapperStyle={{ justifyContent: 'space-between' }}
+                    numColumns={2}
+                    style={styles.list}
+                    data={this.state.characters}
+                    renderItem={({ item, index }) => {
 
-				 numColumns={2}
-					style={styles.list}
-					data={this.state.characters}
-					renderItem={({ item, index }) => {
-						return (
-							<View key={index} style={styles.container}>
-								<Image source={{ uri: item.image }} style={styles.image} />
-								<Text style={[styles.text]}>{item.name}</Text>
-							</View>
+                        return (
+                            
+                            <TouchableOpacity onPress={() => 'DetailScreen'}>
+    
+                                <View key={index} style={styles.container}>
+                                    <Image source={{ uri: item.image }} style={styles.image} />
+                                    <Text style={[styles.text]}>{item.name}</Text>
+                                </View>
+                              
+                            </TouchableOpacity>
+                            
+                        );
+                        
+                    }}
+                />
+            </View>
 
-						)
-					}}
+            
+        );
 
-				/>
-			</View>
-		);
-
-	}
+    }
 }
 
 
+
+
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: "column",
-		marginTop: 15,
-		paddingHorizontal: 10
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        marginTop: 15,
+        paddingHorizontal: 10
 
-	},
-	list: {
-		flex: 1,
-		flexDirection: "column",
+    },
+    list: {
+        flex: 1,
+        flexDirection: "column",
 
-	},
-	image: {
-		width: 180,
-		height: 180,
-		borderRadius: 25
-	
+    },
+    image: {
+        width: 180,
+        height: 180,
+        borderRadius: 25
 
-	},
-	row: {
-		flexDirection: "row",
-		flexWrap: "wrap",
 
-	},
-	text: {
-		flex: 1,
-		fontSize: 18,
-		marginTop: 5,
-		color: "black",
-		fontFamily: "Biryani-Bold"
-	},
+    },
+    row: {
+        flexDirection: "row",
+        flexWrap: "wrap",
 
-	header: {
-		fontSize: 30,
-		marginLeft: 10,
-		color: "black",
-		fontFamily: "Biryani-Bold"
-	}
+    },
+    text: {
+        flex: 1,
+        fontSize: 18,
+        marginTop: 5,
+        color: "black",
+        fontFamily: "Biryani-Bold"
+    },
+
+    header: {
+        fontSize: 30,
+        marginLeft: 10,
+        color: "black",
+        fontFamily: "Biryani-Bold"
+    }
 })
+
 
 export default App;
---
+++ screen/DetailScreen.js
@@ -0,0 +1,40 @@
+
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+  FlatList
+} from 'react-native';
+
+
+
+
+const App = () => {
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Blablalalalalalalalalalağğğğ</Text>
+    </SafeAreaView>
+
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+
+
+
+
+export default App;
+
